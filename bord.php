6ddbca10d59d8091fa1e1d5913ea05e88390b479
kb995 <t.kuboshima995@gmail.com>
2020-03-13 23:17:43 +0900

    scss 追加

--- bord.php
+++ bord.php
@@ -64,17 +64,17 @@ if(!empty($_POST)) {
     <!-- メッセージフォーム -->
     <h1 class="page-title">みんなのひとりごと</h1>
     <form class="form" action="" method="post" enctype="multipart/form-data">
-    <!-- --------------- bordのスタイルシート ------------------ -->
-        <p class="bg-light rounded">
-            <p><?php echo $db_user['user_name']; ?></p>
+        <p class="bord-img">
             <?php if(!empty($db_user['thumbnail'])): ?>
                 <img src="<?php echo 'img/' . $db_user['thumbnail']; ?>" alt="あなたのプロフィール画像">
             <?php else: ?>
                 <img src="<?php echo 'img/' . $default_img; ?>" alt="デフォルト画像">
             <?php endif; ?>
         </p>
-        <div class="form-group mb-5">
-            <label class="control-label" for="">ひとりごとをつぶやく</label>
+        <p class="bord-name"><?php echo $db_user['user_name']; ?></p>
+        <hr>
+        <div class="form-group">
+            <label class="control-label text-label" for="">ひとりごとをつぶやく</label>
             <textarea class="form-control" name="message" rows="8" cols="40"><?php if(!empty($_POST['message'])) echo $_POST['message']; ?></textarea>
             <p class="err_msg">
                 <?php if(!empty($err_msg['message'])) echo $err_msg['message'];  ?>
@@ -82,55 +82,60 @@ if(!empty($_POST)) {
         </div>
         <!-- 画像アップロード -->
         <div class="form-group">
-            <label class="control-label" for="">画像の添付</label>
-            <input class="form-control" type="file" name="upload">
+            <label class="control-label text-label" for="">画像の添付</label>
+            <input class="form-control input-file" type="file" name="upload">
             <p class="err_msg">
                 <?php if(!empty($err_msg['image'])) echo $err_msg['image'];  ?>
             </p>
         </div>
-        <div class="text-center">
-            <input type="submit" class="button" value="投稿する">
+        <div class="text-center my-5">
+            <input type="submit" class="button-post button" value="投稿する">
         </div>
     </form>
 
     <!-- 検索フォーム -->
-    <form class="bg-light col-7 mx-auto my-5 p-5 center" action="search.php" method="get">
-    <div class="form-group mb-5 row">
+    <form class="form my-5" action="search.php" method="get">
+    <div class="form-group search-form row">
         <input class="form-control col-9" type="text" name="search">
-        <input class="btn-info col-2" type="submit" value="検索">
+        <input class="button-search col-3" type="submit" value="検索">
     </div>
     </form>
 
     <!-- メッセージ表示領域 -->
-    <section style="width:70%; min-height: 1000px;" class="mx-auto my-5 p-5 bg-light">
+    <section class="bord">
         <?php foreach($messages as $data): ?>
-            <div style="overflow:hidden; width: 100px; height: 100px; background-color: white;">
+            <p class="bord-thumbnail">
                 <?php if(!empty($data['thumbnail'])): ?>
                     <a href="profDetail.php?user_id=<?php echo $data['user_id']; ?>">
-                        <img class="block" style="width: 100%; height:auto;" src="<?php echo 'img/' . $data['thumbnail']; ?>" alt="あなたのプロフィール画像">
+                        <img src="<?php echo 'img/' . $data['thumbnail']; ?>" alt="あなたのプロフィール画像">
                     </a>
                 <?php else: ?>
-                    <img style="width:100px;height:100px;" src="<?php echo 'img/' . $default_img; ?>" alt="デフォルト画像">
+                    <img src="<?php echo 'img/' . $default_img; ?>" alt="デフォルト画像">
                 <?php endif; ?>
-            </div>
-            <p class="p-2"><?php echo $data['user_name']; ?></p>
-            <span class="pr-3"><?php echo $data['create_date']; ?></span>
-            <?php if($_SESSION['user_id'] == $data['user_id']): ?>
-                <a href="delete.php?message_id=<?php echo $data['id']; ?>">削除</a>
-                <a href="editMessage.php?message_id=<?php echo $data['id']; ?>" class="pr-3">編集</a>
-            <?php endif; ?>
-
-            <div class="mb-5" style="border-radius: 8px; background-color: #f1f1f1; color: #555;">
-                 <p class="p-3 my-3"><?php echo $data['message']; ?></p>
-                <?php if(!empty($data['upload_img'])): ?>
-                <p class="mx-auto" style="width: 400px;height:400px;"><img style="max-width:100%;" src="upload_img/<?php echo $data['upload_img']; ?>" alt="アップロードイメージ"></p>
+            </p>
+            <a href="profDetail.php?user_id=<?php echo $data['user_id']; ?>">
+                <p class="username"><?php echo $data['user_name']; ?></p>
+            </a>
+            <p class="message-info">
+                <span><?php echo $data['create_date']; ?></span>
+                <?php if($_SESSION['user_id'] == $data['user_id']): ?>
+                    <a class="edit-link" href="editMessage.php?message_id=<?php echo $data['id']; ?>" class="pr-3">編集</a>
+                    <a class="delete-link" href="delete.php?message_id=<?php echo $data['id']; ?>">削除</a>
+                <?php endif; ?>
+            </p>
+            <div class="message-content">
+                <p class="message-text">
+                    <?php echo $data['message']; ?>
+                </p>
+                <?php if(isset($data['upload_img'])): ?>
+                    <img class="upload-img" src="upload_img/<?php echo $data['upload_img']; ?>" alt="アップロードイメージ">
                 <?php endif; ?>
             </div>
             <hr class="my-5">
         <?php endforeach; ?>
         <!-- ページング -->
         <?php if($page >= 2): ?>
-            <a class="pl-5" href="bord.php?page=<?php echo $page - 1; ?>">&lt;<?php echo $page - 1; ?>ページ目</a>
+            <a class="pl-5 paging" href="bord.php?page=<?php echo $page - 1; ?>">&lt;<?php echo $page - 1; ?>ページ目</a>
         <?php endif; ?>
         <?php
             $dbh = dbConnect();
@@ -139,7 +144,7 @@ if(!empty($_POST)) {
             $max_page = ceil($page_count['COUNT(*)'] / 5);
             if($page < $max_page):
         ?>
-        <a class="pl-5" href="bord.php?page=<?php echo $page + 1; ?>"><?php echo $page + 1; ?>ページ目&gt;</a>
+        <a class="pl-5 paging" href="bord.php?page=<?php echo $page + 1; ?>"><?php echo $page + 1; ?>ページ目&gt;</a>
         <?php endif; ?>
     </section>
 </main>
